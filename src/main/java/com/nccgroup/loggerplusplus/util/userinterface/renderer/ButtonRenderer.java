d4799fc2884bb05493bc6ad6f956a617f75d77c8
Avneesh Hota <avneesh@akto.io>
2023-01-23 11:56:42 +0530

    added utils back

---
+++ src/main/java/com/nccgroup/loggerplusplus/util/userinterface/renderer/ButtonRenderer.java
@@ -0,0 +1,18 @@
+package com.nccgroup.loggerplusplus.util.userinterface.renderer;
+
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+
+/**
+ * Created by corey on 22/08/17.
+ */
+public class ButtonRenderer implements TableCellRenderer {
+    @Override public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        JButton button = (JButton)value;
+        button.setOpaque(false);
+        button.setForeground(table.getForeground());
+        button.setBackground(UIManager.getColor("Button.background"));
+        return button;
+    }
+}
